74165283f8895c513e69a2ffc022363a6b1ec40b
r <inksomnia6661@gmail.com>
2025-12-19 17:39:22 +0100

    converted to py

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,82 @@
+#include <cstdlib>
 #include <iostream>
 #include <string>
+#include <string_view>
 #include <vector>
+#include <filesystem>
 
-int main(int argc, char** argv) {
-    std::string emsdk;
+std::string get_env_var(const char* name) {
+#if defined(_MSC_VER)
+    char *buf = nullptr;
+    size_t sz = 0;
+    if (_dupenv_s(&buf, &sz, name) == 0 && buf != nullptr) {
+        std::string val = buf;
+        free(buf);
+        return val;
+    }
+    return "";
+#else
+    char* val = getenv(name);
+    return val ? std::string(val) : "";
+#endif
+}
 
-    #if defined(_MSC_VER)
-        char* tmp = nullptr;
-        size_t len = 0;
-        _dupenv_s(&tmp, &len, "EMSDK");
-        if(tmp) { emsdk = tmp; free(tmp); }
-    #else
-        char* tmp = getenv("EMSDK");
-        if(tmp) emsdk = tmp;
-    #endif
+int main(int argc, char **argv) {
+    std::string emsdk = get_env_var("EMSDK");
 
-    std::string emcmake;
-    if(!emsdk.empty()) {
-        emcmake = emsdk + "/upstream/emscripten/emcmake.ps1";
+    // using py because .ps1 & .bat always fail because of having to escape all the " ' . : etc
+    // and just for easier x-platform compatibility
+    std::string emcmake_script;
+    if (!emsdk.empty()) {
+        emcmake_script = emsdk + "/upstream/emscripten/emcmake.py";
     } else {
-        emcmake = "emcmake";
+        emcmake_script = "emcmake.py"; 
     }
 
-    std::string pwsh;
-    #if defined(_MSC_VER)
-        char* tmp2 = nullptr; size_t len2 = 0;
-        _dupenv_s(&tmp2, &len2, "PWSH");
-        if(tmp2) { pwsh = tmp2; free(tmp2); }
-    #endif
-        if(pwsh.empty()) pwsh = "pwsh";
+    std::string python_exe = get_env_var("EMSDK_PYTHON");
+    if (python_exe.empty()) {
+        python_exe = "python";
+    }
 
-    // build command safely
+    bool probe = false;
+    for (int i = 1; i < argc; i++) {
+        std::string_view a = argv[i];
+        if (a == "--version" || a == "-E") {
+            probe = true;
+            break;
+        }
+    }
+
     std::vector<std::string> cmdParts;
-    if(!emsdk.empty()) {
-        cmdParts.push_back(pwsh);
-        cmdParts.push_back("-NoProfile");
-        cmdParts.push_back("-ExecutionPolicy Bypass");
-        cmdParts.push_back("-File \"" + emcmake + '"');
+
+    if (probe) {
+        cmdParts.push_back("cmake");
+        for (int i = 1; i < argc; ++i) cmdParts.push_back(argv[i]);
     } else {
-        cmdParts.push_back(emcmake);
+        cmdParts.push_back(python_exe);
+        cmdParts.push_back("\"" + emcmake_script + "\"");
+        cmdParts.push_back("cmake");
+
+        for (int i = 1; i < argc; ++i) {
+            std::string arg = argv[i];
+            
+            if (arg.find(' ') != std::string::npos) {
+                cmdParts.push_back("\"" + arg + "\"");
+            } else {
+                cmdParts.push_back(arg);
+            }
+        }
     }
-
-    for(int i=1;i<argc;i++)
-        cmdParts.push_back('"' + std::string(argv[i]) + '"');
-
-    for(int i = 1; i < argc; ++i)
-        cmdParts.push_back('"' + std::string(argv[i]) + '"');
 
     std::string cmd;
-    for(const auto& part : cmdParts) {
-        if(!cmd.empty()) cmd += " ";
+    for (const auto &part : cmdParts) {
+        if (!cmd.empty()) cmd += " ";
         cmd += part;
     }
 
-    std::cout << "DEBUG: " << cmd << "\n";
+#ifdef DBG
+    std::cout << "[emshim] full cmd: " << cmd << std::endl;
+#endif
+
     return system(cmd.c_str());
 }
